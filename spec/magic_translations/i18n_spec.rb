a82d1eb3e01f107deb4afee1d734ace9bdd3fe67
Lunar <lunar@anargeek.net>
2012-02-20 12:31:36 +0100

    Use non-interpolated heredocs in i18n specs

--- spec/magic_translations/i18n_spec.rb
+++ spec/magic_translations/i18n_spec.rb
@@ -12,33 +12,33 @@ describe 'Haml magic translations with I18n' do
   it 'should translate text using existing locales' do
     Haml::Template.options[:magic_translations] = true
     I18n.locale = :pl
-    <<-HTML.strip_heredoc.should == render(<<-HAML.strip_heredoc)
+    <<-'HTML'.strip_heredoc.should == render(<<-'HAML'.strip_heredoc)
       <p>Magiczne tłumaczenie działa!</p>
       <p>A tutaj razem z interpolacją, a to wszystko dzięki połączeniu I18n i GetText</p>
     HTML
       %p Magic translations works!
-      %p Here with interpolation, and everything thanks to \#{I18n.name} and \#{GetText.name}
+      %p Here with interpolation, and everything thanks to #{I18n.name} and #{GetText.name}
     HAML
   end
 
   it 'should leave text without changes when translation was not found' do
     Haml::Template.options[:magic_translations] = true
     I18n.locale = :en
-    <<-HTML.strip_heredoc.should == render(<<-HAML.strip_heredoc)
+    <<-'HTML'.strip_heredoc.should == render(<<-'HAML'.strip_heredoc)
       <p>Magic translations works!</p>
       <p>And this one does not exist</p>
       <p>Here with interpolation, and everything thanks to I18n and GetText</p>
     HTML
       %p Magic translations works!
       %p And this one does not exist
-      %p Here with interpolation, and everything thanks to \#{I18n.name} and \#{GetText.name}
+      %p Here with interpolation, and everything thanks to #{I18n.name} and #{GetText.name}
     HAML
   end
 
   it 'should translate text with multiline plain text' do
     Haml::Template.options[:magic_translations] = true
     I18n.locale = :pl
-    <<-HTML.strip_heredoc.should == render(<<-HAML.strip_heredoc)
+    <<-'HTML'.strip_heredoc.should == render(<<-'HAML'.strip_heredoc)
       <p>Magiczne tłumaczenie działa!</p>
       <p>
         Kolejny wieloliniowy tekst,
@@ -50,13 +50,13 @@ describe 'Haml magic translations with I18n' do
       %p
         Now we will check multiline strings,
         which should be also translated,
-        with interpolation \#{'Interpolation'.upcase}
+        with interpolation #{'Interpolation'.upcase}
     HAML
   end
 
   it 'should leave text without changes when :magic_translations option is off' do
     Haml::Template.options[:magic_translations] = false
-    <<-HTML.strip_heredoc.should == render(<<-HAML.strip_heredoc)
+    <<-'HTML'.strip_heredoc.should == render(<<-'HAML'.strip_heredoc)
       <p>Text without changes</p>
     HTML
       %p Text without changes
